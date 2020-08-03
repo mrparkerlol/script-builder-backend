4946ae2aa3ae73e30ebaf49ed31a8dd1600afa0c
Jacob <2037716+mrteenageparker@users.noreply.github.com>
2020-08-02 20:33:39 -0700

    Update Upload XML
    Change XML for the new way the SB works.

--- index.js
+++ index.js
@@ -5,18 +5,51 @@ async function generateCodeBody(code) {
   <Meta name="ExplicitAutoJoints">true</Meta>
   <External>null</External>
   <External>nil</External>
-  <Item class="LocalScript" referent="RBX75A213463503497796DE9DF31173BC1A">
-    <Properties>
-      <BinaryString name="AttributesSerialize"></BinaryString>
-      <bool name="Disabled">true</bool>
-      <Content name="LinkedSource"><null></null></Content>
-      <string name="Name">LocalScript</string>
-      <string name="ScriptGuid">{db92dbf3-6bd6-4d3c-83c9-cc181ef77722}</string>
-      <ProtectedString name="Source"><![CDATA[` + code + `]]></ProtectedString>
-      <BinaryString name="Tags"></BinaryString>
-    </Properties>
+  <Item class="LocalScript" referent="RBX2160BD1F826C450A8F2B7E3AC77849C9">
+	  <Properties>
+		  <BinaryString name="AttributesSerialize"></BinaryString>
+		  <bool name="Disabled">true</bool>
+		  <Content name="LinkedSource"><null></null></Content>
+		  <string name="Name">LocalScript</string>
+		  <string name="ScriptGuid">{0E46D412-3104-4684-9119-9515C1822CF2}</string>
+		  <ProtectedString name="Source"><![CDATA[local config = shared(script, getfenv());
+local environment = config and config.environment;
+
+local success, source = pcall(function()
+	return require(script:WaitForChild("LSource"));
+end);
+
+script:ClearAllChildren();
+
+if success then
+	setfenv(0, environment);
+	setfenv(1, environment);
+	setfenv(source, environment);
+	spawn(function()
+		shared("Output", {
+			Type = "general",
+			Message = "Ran local script",
+		});
+
+		source();
+	end);
+else
+	error(source, 0);
+end;]]></ProtectedString>
+		  <BinaryString name="Tags"></BinaryString>
+	  </Properties>
+	  <Item class="ModuleScript" referent="RBX066B198861FE488184CF42EEF7F3A894">
+		  <Properties>
+			  <BinaryString name="AttributesSerialize"></BinaryString>
+			  <Content name="LinkedSource"><null></null></Content>
+			  <string name="Name">LSource</string>
+			  <string name="ScriptGuid">{85DF8BE5-8F00-4F18-9A2F-70F141465182}</string>
+			  <ProtectedString name="Source"><![CDATA[return function()` + code + ` end]]></ProtectedString>
+			  <BinaryString name="Tags"></BinaryString>
+		  </Properties>
+	  </Item>
   </Item>
-  </roblox>`;
+</roblox>`;
 }
 
 addEventListener('fetch', event => {
