965f21eceecb3095334553a4658052f569588847
Jacob <2037716+mrteenageparker@users.noreply.github.com>
2020-08-03 19:25:57 -0700

    Lay initial groundwork for saving scripts, re-orient the way secrets are stored, update README

--- README.md
+++ README.md
@@ -6,8 +6,24 @@ https://github.com/mrteenageparker/script-builder
 
 This handles the local scripts, as well as sandboxing module scripts (coming soon).
 
-Uses the following dependencies:
+Requires the following prerequisites:
+
 - A Cloudflare account (https://cloudflare.com)
 - A Cloudflare Workers worker (free - 100k requests a day - https://workers.dev)
-- Wrangler CLI tool (https://developers.cloudflare.com/workers/tooling/wrangler/install/)
-- Rust or Node toolchains (required by Wrangler) (https://www.rust-lang.org/ or https://nodejs.org/en/)
+- Wrangler CLI tool (https://developers.cloudflare.com/workers/tooling/wrangler/install)
+- Rust or Node toolchains (required by Wrangler) (https://www.rust-lang.org or https://nodejs.org/en)
+- NodeJS for installing dependencies (https://nodejs.org/en)
+- A FaunaDB account (https://fauna.com)
+
+The project has the following dependencies:
+
+- FaunaDB (https://www.npmjs.com/package/faunadb)
+
+An example config.js:
+
+```
+module.exports = {
+	robloxSecret: "", // .ROBLOSECURITY cookie for alternate account
+	fauanaSecret: "" // Secret for faunaDB
+}
+```
--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const rbxCookie = ""; // Insert your (non-main account) account's cookie here
+const config = require('./config');
+const rbxCookie = config.robloxSecret;
+const faunaSecret = config.faunaSecret;
+
+var faunadb = require('faunadb'),
+	q = faunadb.query;
+
+var client = new faunadb.Client({
+	secret: faunaSecret,
+	fetch: fetch.bind(globalThis)
+});
 
 async function generateCodeBody(code) {
-  return `<roblox xmlns:xmime="http://www.w3.org/2005/05/xmlmime" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.roblox.com/roblox.xsd" version="4">
-  <Meta name="ExplicitAutoJoints">true</Meta>
-  <External>null</External>
-  <External>nil</External>
-  <Item class="LocalScript" referent="RBX2160BD1F826C450A8F2B7E3AC77849C9">
-	  <Properties>
-		  <BinaryString name="AttributesSerialize"></BinaryString>
-		  <bool name="Disabled">true</bool>
-		  <Content name="LinkedSource"><null></null></Content>
-		  <string name="Name">LocalScript</string>
-		  <string name="ScriptGuid">{0E46D412-3104-4684-9119-9515C1822CF2}</string>
-		  <ProtectedString name="Source"><![CDATA[local config = shared(script, getfenv());
+	return `<roblox xmlns:xmime="http://www.w3.org/2005/05/xmlmime" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.roblox.com/roblox.xsd" version="4">
+<Meta name="ExplicitAutoJoints">true</Meta>
+<External>null</External>
+<External>nil</External>
+<Item class="LocalScript" referent="RBX2160BD1F826C450A8F2B7E3AC77849C9">
+<Properties>
+<BinaryString name="AttributesSerialize"></BinaryString>
+<bool name="Disabled">true</bool>
+<Content name="LinkedSource"><null></null></Content>
+<string name="Name">LocalScript</string>
+<string name="ScriptGuid">{0E46D412-3104-4684-9119-9515C1822CF2}</string>
+<ProtectedString name="Source"><![CDATA[local config = shared(script, getfenv());
 local environment = config and config.environment;
 
 local success, source = pcall(function()
@@ -36,62 +46,136 @@ if success then
 else
 	error(source, 0);
 end;]]></ProtectedString>
-		  <BinaryString name="Tags"></BinaryString>
-	  </Properties>
-	  <Item class="ModuleScript" referent="RBX066B198861FE488184CF42EEF7F3A894">
-		  <Properties>
-			  <BinaryString name="AttributesSerialize"></BinaryString>
-			  <Content name="LinkedSource"><null></null></Content>
-			  <string name="Name">LSource</string>
-			  <string name="ScriptGuid">{85DF8BE5-8F00-4F18-9A2F-70F141465182}</string>
-			  <ProtectedString name="Source"><![CDATA[return function()` + code + ` end]]></ProtectedString>
-			  <BinaryString name="Tags"></BinaryString>
-		  </Properties>
-	  </Item>
-  </Item>
+<BinaryString name="Tags"></BinaryString>
+</Properties>
+<Item class="ModuleScript" referent="RBX066B198861FE488184CF42EEF7F3A894">
+<Properties>
+<BinaryString name="AttributesSerialize"></BinaryString>
+<Content name="LinkedSource"><null></null></Content>
+<string name="Name">LSource</string>
+<string name="ScriptGuid">{85DF8BE5-8F00-4F18-9A2F-70F141465182}</string>
+<ProtectedString name="Source"><![CDATA[return function()` + code + ` end]]></ProtectedString>
+<BinaryString name="Tags"></BinaryString>
+</Properties>
+</Item>
+</Item>
 </roblox>`;
 }
 
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
+	event.respondWith(handleRequest(event.request));
 });
 
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+async function getData(index, object) {
+	try {
+		var result = await client.query(
+			q.Get(
+				q.Match(
+					q.Index(index), object
+				)
+			)
+		);
+
+		return result.data ? result.data : null;
+	} catch(ex) {
+		return false;
+	}
+}
+
 async function handleRequest(request) {
-  const url = new URL(request.url);
-  const method = request.method;
-  if (method == "POST") {
-    if (url.pathname == "/post/uploadScript") {
-      try {
-        const bodyUsed = await request.json();
-        const code = await generateCodeBody(bodyUsed.code.toString());
-        const assetId = bodyUsed.assetId.toString();
-        const resp = await fetch(new Request("https://data.roblox.com/Data/Upload.ashx?json=1&assetid=" + assetId), {
-          method: "POST",
-          body: code,
-          headers: {
-            "cookie": ".ROBLOSECURITY=" + rbxCookie,
-            "content-type": "application/xml",
-            "content-length": code.length,
-            "connection": "keep-alive",
-          }
-        });
+	const url = new URL(request.url);
+	const method = request.method;
+	if (method == "POST") {
+		if (url.pathname == "/post/uploadScript") {
+			try {
+				const bodyUsed = await request.json();
+				const code = await generateCodeBody(bodyUsed.code.toString());
+				const assetId = bodyUsed.assetId.toString();
+				const resp = await fetch(new Request("https://data.roblox.com/Data/Upload.ashx?json=1&assetid=" + assetId), {
+					method: "POST",
+					body: code,
+					headers: {
+						"cookie": ".ROBLOSECURITY=" + rbxCookie,
+						"content-type": "application/xml",
+						"content-length": code.length,
+						"connection": "keep-alive",
+					}
+				});
 
-        return new Response(await resp.text(), {
-          headers: { 'content-type': 'text/html' },
-        });
-      } catch(ex) {
-        return new Response(ex, {
-          headers: { 'content-type': 'text/json' },
-        });
-      }
-    }
-  }
+				return new Response(await resp.text(), {
+					headers: { 'content-type': 'text/html' },
+				});
+			} catch(ex) {
+				return new Response("Error occured when posting to Roblox backend", {
+					headers: { 'content-type': 'text/plain' },
+				});
+			}
+		} else if (url.pathname == "/post/saveScript") {
+			try {
+				const bodyUsed = await request.json();
+				const userId = parseInt(bodyUsed.userId);
+				const scriptName = bodyUsed.scriptName;
+				const code = bodyUsed.code;
 
-  return new Response('<b>API backend</b>', {
-    headers: { 'content-type': 'text/html' },
-  });
+				if (userId != NaN) {
+					let exists = await getData("findScript", [scriptName, userId]);
+					exists = exists ? true : false;
+					if (!exists) {
+						await client.query(
+							q.Create(
+								q.Collection('scripts'),
+								{
+									data: {
+										userId: userId,
+										scriptName: scriptName,
+										code: code
+									}
+								}
+							)
+						);
+
+						return new Response("Successfully saved script", {
+							headers: { "content-type": "text/plain" },
+						});
+					}
+
+					return new Response("Error: script already exists", {
+						status: 500,
+						headers: { "content-type": "text/plain" },
+					});
+				}
+			} catch(ex) { }
+		} else if (url.pathname == "/post/getScript") {
+			try {
+				const bodyUsed = await request.json();
+				const userId = parseInt(bodyUsed.userId);
+				const scriptName = bodyUsed.scriptName;
+
+				if (userId != NaN) {
+					let result = await getData("findScript", [scriptName, userId]);
+					result = result ? { found: true, result: result } : { found: false, result: null };
+					return new Response(JSON.stringify(result), {
+						headers: {
+							"content-type": "text/json"
+						}
+					});
+				} else {
+					return new Response("Error converting UserId to integer", {
+						headers: {
+							"content-type": "text/plain"
+						}
+					});
+				}
+			} catch(ex) {
+				return new Response(ex, {
+					status: 500,
+					headers: { "content-type": "text-plain" },
+				});
+			}
+		}
+	}
+
+	return new Response('<h1>API backend</h1>', {
+		headers: { 'content-type': 'text/html' },
+	});
 }
